a7dc1ec1ded0a6b14a8e0c037c27739e4a567f11
dymont78 <tkatchuk.dmitry@gmail.com>
2021-05-29 15:51:42 +0300

    Update pom.xml and IConstants.java

--- src/main/java/constants/IConstants.java
+++ src/main/java/constants/IConstants.java
@@ -2,5 +2,5 @@ package constants;
 
 public interface IConstants {
     String LOGIN_URL = "https://www.saucedemo.com/";
-    String CART_URL = "https://www.saucedemo.com/cart.html";
+    String CART_URL = LOGIN_URL + "cart.html";
 }
